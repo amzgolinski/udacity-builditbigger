60c423d4110a9a902070843403e60caae1c33248
Alex Zgolinski <azgolinski@azgolinski-macbookpro2.roam.corp.google.com>
2016-05-01 15:50:54 -0700

    Adding AsyncTest.

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTest.java
@@ -0,0 +1,47 @@
+package com.udacity.gradle.builditbigger;
+
+import android.test.AndroidTestCase;
+
+import java.util.concurrent.TimeUnit;
+
+public class AsyncTest extends AndroidTestCase {
+
+  JokeAsyncTask jokeAsyncTask;
+  String joke;
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    joke = null;
+
+    JokeCallback callback = new JokeCallback() {
+      @Override
+      public void jokeRetrieved(String joke) {
+        // empty
+      }
+    };
+
+    jokeAsyncTask = new JokeAsyncTask(callback) {
+
+      @Override
+      protected void onPostExecute(String joke) {
+        // empty
+      }
+    };
+
+  }
+
+  public void testJokeTaskReturnValue() {
+
+    try {
+      jokeAsyncTask.execute();
+      joke = jokeAsyncTask.get(10, TimeUnit.SECONDS);
+      System.out.println(joke);
+      assertNotNull(joke);
+
+    } catch (Exception e) {
+      fail("Task timed out.");
+    }
+  }
+
+}
